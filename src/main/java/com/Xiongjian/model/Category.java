029a4bda8422d8ec5f07b13e87dd02a62a3f4d46
27143 <2714303349@qq.com>
2021-05-20 13:13:41 +0800

    HW-week11 Commit

---
+++ src/main/java/com/Xiongjian/model/Category.java
@@ -0,0 +1,10 @@
+package com.Xiongjian.model;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+    private String description;
+    private boolean active;
+
+
+}
